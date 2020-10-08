49cabf6f7ec2d64eadbdeedf80faf9cdb9e8cdd1
Pankaja Chalaru <42083827+crusherlk@users.noreply.github.com>
2020-10-09 00:13:02 +0530

    Add files via upload

--- MortgageCal/src/com/company/Main.java
+++ MortgageCal/src/com/company/Main.java
@@ -8,17 +8,37 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        int principal = 0;
+        while (true) {
+            System.out.print("Principal ($1K-$1M): ");
+            principal = scanner.nextInt();
+            if (principal >=1000 && principal<=1000000)
+                break;
+            System.out.println("Enter a value $1K-$1M");
+        }
 
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
+        float interest;
+        float monthlyInterest;
+        while (true) {
+            System.out.print("Annual Interest Rate: ");
+            interest= scanner.nextFloat();
+            monthlyInterest = interest/12/100;
+            if (interest >=1 && interest<=30)
+                break;
+            System.out.println("Enter a value 1%-30%");
+        }
 
-        System.out.print("Annual Interest Rate: ");
-        float interest = scanner.nextFloat();
-        float monthlyInterest = interest/12/100;
 
-        System.out.print("Period (years): ");
-        int period = scanner.nextInt();
-        int totPays = period*12;
+        int period=0;
+        int totPays;
+        while (true) {
+            System.out.print("Period (years): ");
+            period = scanner.nextInt();
+            totPays = period*12;
+            if (period >=1 && period<=30)
+                break;
+            System.out.println("Enter a value 1-30");
+        }
 
         double mortgage;
 
